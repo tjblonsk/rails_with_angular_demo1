f1bb376abb963b01e021da08e8356054ea97d6b6
Tim <tim.blonski@gmail.com>
2013-10-17 12:38:41 -0400

    added angular and basic controller and view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '5ef65a419805b4344d360fefc3b45415e458277591f7fbe1aa015c61d020a6c44db434e27f291924e56076f805a7d1905d6c239386f4ccdd9f45ee71fc7734ef'
